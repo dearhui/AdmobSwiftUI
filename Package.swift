be582610aad792487c4c712aef681fbbbcd63f9b
Tal Aviram <me@talaviram.com>
2024-03-19 09:50:12 +0200

    Use newer swift-package-manager-google-mobile-ads to fix build with newer Xcode.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "10.0.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.2.0"),
         .package(url: "https://github.com/bhlvoong/LBTATools", from: "1.0.17"),
     ],
     targets: [
